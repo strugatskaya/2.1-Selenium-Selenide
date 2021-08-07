247069d43581bad18543d9381d8f8dc94e1c39f9
Juliya Strugatskaya <strugatskaya.um@gmail.com>
2021-08-07 19:32:53 +0300

    Добавлен забытый Гитигнор, полностью переделаны тесты, которые падают все равно

--- src/test/java/AlphaBankFormTestChrome.java
+++ src/test/java/AlphaBankFormTestChrome.java
@@ -1,8 +1,5 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -16,13 +13,11 @@ public class AlphaBankFormTestChrome {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "/driver/win/chromedriver");
         WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     void setUp() {
-        driver = new ChromeDriver();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
@@ -38,79 +33,176 @@ public class AlphaBankFormTestChrome {
     }
 
     @Test
-    void shouldTestFindElementAndSendKeysOnChromePositive1() {
+    void shouldTestAllFieldsIsCorrectPositive1() {
         driver.get("http://localhost:9999");
         List<WebElement> textField = driver.findElements(By.className("input__control"));
         textField.get(0).sendKeys("Понд Эми");
-        textField.get(0).sendKeys("+39999999999");
+        textField.get(1).sendKeys("+39999999999");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
     @Test
-    void shouldTestFindElementAndSendKeysOnChromePositive2() {
+    void shouldTestPositiveAllFieldsIsCorrectWithDashPositive() {
         driver.get("http://localhost:9999");
         List<WebElement> textField = driver.findElements(By.className("input__control"));
         textField.get(0).sendKeys("Клара Освин-Освальд");
-        textField.get(0).sendKeys("+00000000000");
+        textField.get(1).sendKeys("+70000000000");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
     @Test
-    void shouldTestFindElementAndSendKeysOnChromeNegative1() {
+    void shouldTestWithLatinLettersNameNegative() {
         driver.get("http://localhost:9999");
         List<WebElement> textField = driver.findElements(By.className("input__control"));
         textField.get(0).sendKeys("The Doctor");
-        textField.get(0).sendKeys("+3999999999");
+        textField.get(1).sendKeys("+00000000000");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();
+        Assertions.assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
     }
 
     @Test
-    void shouldTestFindElementAndSendKeysOnChromeNegative2() {
+    void shouldTestWithTenTelNumbersNegative() {
+        driver.get("http://localhost:9999");
+        List<WebElement> textField = driver.findElements(By.className("input__control"));
+        textField.get(0).sendKeys("The Doctor");
+        textField.get(1).sendKeys("+0000000000");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();
+        Assertions.assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
+
+    @Test
+    void shouldTestWithNumbersInNameNegative() {
         driver.get("http://localhost:9999");
         List<WebElement> textField = driver.findElements(By.className("input__control"));
         textField.get(0).sendKeys("Понд Эми6");
-        textField.get(0).sendKeys("39999999999");
-    }
-
-    @Test
-    void shouldTestFindElementAndSendKeysOnChromeNegative3() {
-        driver.get("http://localhost:9999");
-        List<WebElement> textField = driver.findElements(By.className("input__control"));
-        textField.get(0).sendKeys("");
-        textField.get(0).sendKeys("");
-    }
-
-    @Test
-    void shouldTestFindElementAndSendKeysOnChromeNegative4() {
-        driver.get("http://localhost:9999");
-        List<WebElement> textField = driver.findElements(By.className("input__control"));
-        textField.get(0).sendKeys("48");
-        textField.get(0).sendKeys("тардисфон");
-
-    }
-
-    @Test
-    void shouldTestFindElementAndSendKeysOnChromeNegative5() {
-        driver.get("http://localhost:9999");
-        List<WebElement> textField = driver.findElements(By.className("input__control"));
-        textField.get(0).sendKeys("Роуз");
-        textField.get(0).sendKeys("+399999999999");
-
-    }
-
-    @Test
-    void shouldTestFindElementAndSendKeysOnChromeNegative6() {
-        driver.get("http://localhost:9999");
-        List<WebElement> textField = driver.findElements(By.className("input__control"));
-        textField.get(0).sendKeys("   ");
-        textField.get(0).sendKeys("   ");
-
-    }
-
-    @Test
-    void shouldTestFindElementAndClickOnChrome() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.className("checkbox__box")).click();
+        textField.get(1).sendKeys("+39999999999");
+        driver.findElement(By.className("checkbox")).click();
         driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();
+        Assertions.assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
     }
 
+    @Test
+    void shouldTestWithNoPlusIntelNegative() {
+        driver.get("http://localhost:9999");
+        List<WebElement> textField = driver.findElements(By.className("input__control"));
+        textField.get(0).sendKeys("Понд Эми");
+        textField.get(1).sendKeys("39999999999");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();
+        Assertions.assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
+
+    @Test
+    void shouldTestWithNoNameNegative() {
+        driver.get("http://localhost:9999");
+        List<WebElement> textField = driver.findElements(By.className("input__control"));
+        textField.get(0).sendKeys("");
+        textField.get(1).sendKeys("+39999999999");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();//
+        Assertions.assertEquals("Поле обязательно для заполнения", text.trim());
+    }
+
+    @Test
+    void shouldTestWithNoTelNegative() {
+        driver.get("http://localhost:9999");
+        List<WebElement> textField = driver.findElements(By.className("input__control"));
+        textField.get(0).sendKeys("Эми Понд");
+        textField.get(1).sendKeys("");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();//
+        Assertions.assertEquals("Поле обязательно для заполнения.", text.trim());
+    }
+
+    @Test
+    void shouldTestWithNumbersInsteadNameNegative() {
+        driver.get("http://localhost:9999");
+        List<WebElement> textField = driver.findElements(By.className("input__control"));
+        textField.get(0).sendKeys("+0000000000");
+        textField.get(1).sendKeys("+0000000000");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();
+        Assertions.assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+
+    }
+
+    @Test
+    void shouldTestWithLettersInsteadOfTelNegative() {
+        driver.get("http://localhost:9999");
+        List<WebElement> textField = driver.findElements(By.className("input__control"));
+        textField.get(0).sendKeys("Арагорн Элессар");
+        textField.get(1).sendKeys("Арагорн Элессар");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();
+        Assertions.assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+
+    }
+
+    @Test
+    void shouldTestTwelveNumbersInTesNegative() {
+        driver.get("http://localhost:9999");
+        List<WebElement> textField = driver.findElements(By.className("input__control"));
+        textField.get(0).sendKeys("Арагорн");
+        textField.get(1).sendKeys("+399999999999");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();
+        Assertions.assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
+
+    @Test
+    void shouldTestSpacesInsteadOfNameNegative() {
+        driver.get("http://localhost:9999");
+        List<WebElement> textField = driver.findElements(By.className("input__control"));
+        textField.get(0).sendKeys("   ");
+        textField.get(1).sendKeys("+70000000000");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();
+        Assertions.assertEquals("Поле обязательно для заполнения", text.trim());
+
+    }
+
+    @Test
+    void shouldTestSpacesInsteadOfTelNegative() {
+        driver.get("http://localhost:9999");
+        List<WebElement> textField = driver.findElements(By.className("input__control"));
+        textField.get(0).sendKeys("Том Бомбадил");
+        textField.get(1).sendKeys("     ");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();
+        Assertions.assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+
+    }
+//    @Test
+//    void shouldTestFindElementAndSendKeysOnChromeNegativeNoCheckBoxClick() {
+//        driver.get("http://localhost:9999");
+//        List<WebElement> textField = driver.findElements(By.className("input__control"));
+//        textField.get(0).sendKeys("Понд Эми");
+//        textField.get(1).sendKeys("+3999999999");
+//        driver.findElement(By.className("checkbox"));
+//        driver.findElement(By.tagName("button"));
+//        String color = driver.findElement(By.className("checkbox")).getAttribute("#ff5c5c!important");
+//        Assertions.assertEquals("#ff5c5c!important", color);
+//
+//    }
 }
 
 

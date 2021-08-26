14cb7bbf3a08f65ca0ef3b16a0ccad3961bfe2a8
Juliya Strugatskaya <strugatskaya.um@gmail.com>
2021-08-26 20:23:43 +0300

    Добавлен тест Без чекбокса

--- src/test/java/AlphaBankFormTestChrome.java
+++ src/test/java/AlphaBankFormTestChrome.java
@@ -19,12 +19,13 @@ public class AlphaBankFormTestChrome {
 
     @BeforeEach
     void setUp() {
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-
+        driver.get("http://localhost:9999");
     }
 
     @AfterEach
@@ -35,7 +36,6 @@ public class AlphaBankFormTestChrome {
 
     @Test
     void shouldTestAllFieldsIsCorrectPositive1() {
-        driver.get("http://localhost:9999");
         List<WebElement> textField = driver.findElements(By.className("input__control"));
         textField.get(0).sendKeys("Понд Эми");
         textField.get(1).sendKeys("+39999999999");
@@ -47,7 +47,6 @@ public class AlphaBankFormTestChrome {
 
     @Test
     void shouldTestPositiveAllFieldsIsCorrectWithDashPositive() {
-        driver.get("http://localhost:9999");
         List<WebElement> textField = driver.findElements(By.className("input__control"));
         textField.get(0).sendKeys("Клара Освин-Освальд");
         textField.get(1).sendKeys("+70000000000");
@@ -59,7 +58,6 @@ public class AlphaBankFormTestChrome {
 
     @Test
     void shouldTestWithLatinLettersNameNegative() {
-        driver.get("http://localhost:9999");
         List<WebElement> textField = driver.findElements(By.className("input__control"));
         textField.get(0).sendKeys("The Doctor");
         textField.get(1).sendKeys("+00000000000");
@@ -71,7 +69,6 @@ public class AlphaBankFormTestChrome {
 
     @Test
     void shouldTestWithTenTelNumbersNegative() {
-        driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("input[name=name]")).sendKeys("Доктор Тардис");
         driver.findElement(By.cssSelector("input[name=phone]")).sendKeys("+7999999999");
         driver.findElement(By.className("checkbox")).click();
@@ -83,7 +80,6 @@ public class AlphaBankFormTestChrome {
 
     @Test
     void shouldTestWithNumbersInNameNegative() {
-        driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("input[name=name]")).sendKeys("Понд Эми6");
         driver.findElement(By.cssSelector("input[name=phone]")).sendKeys("+10000000000");
         driver.findElement(By.className("checkbox")).click();
@@ -94,8 +90,6 @@ public class AlphaBankFormTestChrome {
 
     @Test
     void shouldTestWithNoPlusIntelNegative() {
-        driver.get("http://localhost:9999");
-
         driver.findElement(By.cssSelector("input[name=name]")).sendKeys("Понд Эми");
         driver.findElement(By.cssSelector("input[name=phone]")).sendKeys("39999999999");
         driver.findElement(By.className("checkbox")).click();
@@ -106,8 +100,6 @@ public class AlphaBankFormTestChrome {
 
     @Test
     void shouldTestWithNoNameNegative() {
-        driver.get("http://localhost:9999");
-
         driver.findElement(By.cssSelector("input[name=name]")).sendKeys("");
         driver.findElement(By.cssSelector("input[name=phone]")).sendKeys("+39999999999");
         driver.findElement(By.className("checkbox")).click();
@@ -118,7 +110,6 @@ public class AlphaBankFormTestChrome {
 
     @Test
     void shouldTestWithNoTelNegative() {
-        driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("input[name=name]")).sendKeys("Эми Понд");
         driver.findElement(By.cssSelector("input[name=phone]")).sendKeys("");
         driver.findElement(By.className("checkbox")).click();
@@ -129,8 +120,6 @@ public class AlphaBankFormTestChrome {
 
     @Test
     void shouldTestWithNumbersInsteadNameNegative() {
-        driver.get("http://localhost:9999");
-
         driver.findElement(By.cssSelector("input[name=name]")).sendKeys("+0000000000");
         driver.findElement(By.cssSelector("input[name=phone]")).sendKeys("+0000000000");
         driver.findElement(By.className("checkbox")).click();
@@ -142,7 +131,6 @@ public class AlphaBankFormTestChrome {
 
     @Test
     void shouldTestWithLettersInsteadOfTelNegative() {
-        driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("input[name=name]")).sendKeys("Арагорн Элессар");
         driver.findElement(By.cssSelector("input[name=phone]")).sendKeys("Арагорн Элессар");
         driver.findElement(By.className("checkbox")).click();
@@ -154,7 +142,6 @@ public class AlphaBankFormTestChrome {
 
     @Test
     void shouldTestTwelveNumbersInTesNegative() {
-        driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("input[name=name]")).sendKeys("Арагорн");
         driver.findElement(By.cssSelector("input[name=phone]")).sendKeys("+399999999999");
         driver.findElement(By.className("checkbox")).click();
@@ -165,7 +152,6 @@ public class AlphaBankFormTestChrome {
 
     @Test
     void shouldTestSpacesInsteadOfNameNegative() {
-        driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("input[name=name]")).sendKeys("   ");
         driver.findElement(By.cssSelector("input[name=phone]")).sendKeys("+70000000000");
         driver.findElement(By.className("checkbox")).click();
@@ -177,7 +163,6 @@ public class AlphaBankFormTestChrome {
 
     @Test
     void shouldTestSpacesInsteadOfTelNegative() {
-        driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("input[name=name]")).sendKeys("Том Бомбадил");
         driver.findElement(By.cssSelector("input[name=phone]")).sendKeys("     ");
         driver.findElement(By.className("checkbox")).click();
@@ -186,6 +171,14 @@ public class AlphaBankFormTestChrome {
         Assertions.assertEquals("Поле обязательно для заполнения", text.trim());
 
     }
+
+    @Test
+    void shouldTestWithoutCheckBox() {
+        driver.findElement(By.cssSelector("input[name=name]")).sendKeys("Галадриэль Лотлориэн");
+        driver.findElement(By.cssSelector("input[name=phone]")).sendKeys("+79236459245");
+        driver.findElement(By.tagName("button")).click();
+        String actualText = driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid .checkbox__text")).getText().trim();
+        String expectedText = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
+        Assertions.assertEquals(expectedText, actualText);
+    }
 }
-
-
